9dcd5cd737a221da56a662d8cd4276b0cd60a5de
Vitor Hugo <vhsilva.ap@gmail.com>
2018-08-19 15:06:16 -0300

    TAREFA 3: Imprimindo a lista de gêneros das primeiras 20 amostras

--- step_by_step.py
+++ step_by_step.py
@@ -30,11 +30,10 @@ input("Aperte Enter para continuar...")
 print("\n\nTAREFA 1: Imprimindo as primeiras 20 amostras")
 
 # Vamos mudar o data_list para remover o cabeçalho dele.
-print(data_list[0])
 data_list = data_list[1:]
 
 for data in data_list[:20]:
-	print(data)
+    print(data)
 
 # Nós podemos acessar as features pelo índice
 # Por exemplo: sample[6] para imprimir gênero, ou sample[-2]
@@ -47,14 +46,38 @@ print("\nTAREFA 2: Imprimindo o gênero das primeiras 20 amostras")
 
 for data in data_list[:20]:
 
-	if data[6] == '':
-		gender = 'uninformed'
-	else:
-		gender = data[6]
+    if data[6] == '':
+        gender = 'uninformed'
+    else:
+        gender = data[6]
 
-	print(gender)
+    print(gender)
 
 # Ótimo! Nós podemos pegar as linhas(samples) iterando com um for, e as colunas(features) por índices.
 # Mas ainda é difícil pegar uma coluna em uma lista. Exemplo: Lista com todos os gêneros
+
+input("Aperte Enter para continuar...")
+# TAREFA 3
+# TODO: Crie uma função para adicionar as colunas(features) de uma lista em outra lista, na mesma ordem
+def column_to_list(data, index):
+    """
+    Function to add columns from a list to another list.
+    :param data: list with the data to be extracted
+    :param index: index of the column you want to get from the data param
+    :returns: new list (column_list) with column data from supplied index
+    """
+    column_list = [column[index] for column in  data]
+    # Dica: Você pode usar um for para iterar sobre as amostras, pegar a feature pelo seu índice, e dar append para uma lista
+    return column_list
+
+# Vamos checar com os gêneros se isso está funcionando (apenas para os primeiros 20)
+print("\nTAREFA 3: Imprimindo a lista de gêneros das primeiras 20 amostras")
+print(column_to_list(data_list, -2)[:20])
+
+# ------------ NÃO MUDE NENHUM CÓDIGO AQUI ------------
+assert type(column_to_list(data_list, -2)) is list, "TAREFA 3: Tipo incorreto retornado. Deveria ser uma lista."
+assert len(column_to_list(data_list, -2)) == 1551505, "TAREFA 3: Tamanho incorreto retornado."
+assert column_to_list(data_list, -2)[0] == "" and column_to_list(data_list, -2)[1] == "Male", "TAREFA 3: A lista não coincide."
+# -----------------------------------------------------
 
 input("Aperte Enter para continuar...")
