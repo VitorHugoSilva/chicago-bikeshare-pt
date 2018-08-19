e7041c8611299a49b8cf66b2850f789fe015d032
Vitor Hugo <vhsilva.ap@gmail.com>
2018-08-19 16:09:23 -0300

    TAREFA 4: Imprimindo quantos masculinos e femininos nós encontramos

--- step_by_step.py
+++ step_by_step.py
@@ -46,10 +46,10 @@ print("\nTAREFA 2: Imprimindo o gênero das primeiras 20 amostras")
 
 for data in data_list[:20]:
 
-    if data[6] == '':
+    if data[-2] == '':
         gender = 'uninformed'
     else:
-        gender = data[6]
+        gender = data[-2]
 
     print(gender)
 
@@ -62,9 +62,12 @@ input("Aperte Enter para continuar...")
 def column_to_list(data, index):
     """
     Function to add columns from a list to another list.
-    :param data: list with the data to be extracted
-    :param index: index of the column you want to get from the data param
-    :returns: new list (column_list) with column data from supplied index
+    :param data: (type :list) list with the data to be extracted
+    :param index: (type :int) index of the column you want to get from the data param
+    :returns: (type :list) new list (column_list) with column data from supplied index
+
+    >>> column_to_list([['data 1','data 2'], ['data 3', 'data 4']], 1))
+        ['data 2', 'data 4']
     """
     column_list = [column[index] for column in  data]
     # Dica: Você pode usar um for para iterar sobre as amostras, pegar a feature pelo seu índice, e dar append para uma lista
@@ -78,6 +81,34 @@ print(column_to_list(data_list, -2)[:20])
 assert type(column_to_list(data_list, -2)) is list, "TAREFA 3: Tipo incorreto retornado. Deveria ser uma lista."
 assert len(column_to_list(data_list, -2)) == 1551505, "TAREFA 3: Tamanho incorreto retornado."
 assert column_to_list(data_list, -2)[0] == "" and column_to_list(data_list, -2)[1] == "Male", "TAREFA 3: A lista não coincide."
+# -----------------------------------------------------
+
+input("Aperte Enter para continuar...")
+# Agora sabemos como acessar as features, vamos contar quantos Male (Masculinos) e Female (Femininos) o dataset tem
+# TAREFA 4
+# TODO: Conte cada gênero. Você não deveria usar uma função parTODO isso.
+def count_gender(data, gender):
+    """
+    Function for counting each gender
+    :param data: (type :list) complete search list
+    :param gender: (type :str) data for count in list
+    :returns: (type :int) number of genders found for (gender)
+
+    >>> count_gender(['Male', 'Male', 'Femile'],  'Male')
+        2
+    """
+    return len([count for count in data if count == gender])
+
+male = count_gender(column_to_list(data_list, -2), 'Male')
+
+female = count_gender(column_to_list(data_list, -2), 'Female')
+
+# Verificando o resultado
+print("\nTAREFA 4: Imprimindo quantos masculinos e femininos nós encontramos")
+print("Masculinos: ", male, "\nFemininos: ", female)
+
+# ------------ NÃO MUDE NENHUM CÓDIGO AQUI ------------
+assert male == 935854 and female == 298784, "TAREFA 4: A conta não bate."
 # -----------------------------------------------------
 
 input("Aperte Enter para continuar...")
